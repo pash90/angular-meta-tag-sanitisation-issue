f55480a66859102c407038242675a5e1d59242e2
Parth Mehta <parth.mehta@invoice2go.com>
2023-06-14 11:53:20 +1000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meta-tag-sanisation-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'meta-tag-sanisation-issue';
+}
